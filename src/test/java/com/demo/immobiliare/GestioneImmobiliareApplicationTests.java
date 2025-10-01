b8529a903d980d3451c303e3513687b50d4c710d
Gabriele Giovannangeli <giovannangeligabriele@gmail.com>
2025-10-01 15:51:35 +0200

    Primo commit

---
+++ src/test/java/com/demo/immobiliare/GestioneImmobiliareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.immobiliare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneImmobiliareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
